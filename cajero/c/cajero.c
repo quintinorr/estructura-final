ecfd6bde34831941ae9f4571f98c8efde6dfcbed
Javier Mercedes <manuelmercedez10@gmail.com>
2019-03-24 13:33:18 -0400

    Modified cajero.c to order numbers

--- cajero/c/cajero.c
+++ cajero/c/cajero.c
@@ -5,4 +5,34 @@ Creado por Javier Mercedes.
 */
 #include <stdio.h>
 
-double MONEDAS[13] = {1000, 2000, 200, 100, 50, 20, 10, 5, 1, (25/100), (10/100), (5/100), (1/100)};
+int main()
+{
+	//Primero, creamos un array con los billetes que vamos a utilizar 	
+	int MONEDAS[9] = {1000, 2000, 200, 100, 50, 20, 10, 5, 1};
+
+	// Creamos la variables a utilizar para ordenar los valores
+	// del array de mayor a menor y para contar la cantidad de 
+	// elementos en MONEDAS[]
+	int i, pos, aux, countArray;
+	
+	// Contamos los elementos en MONEDA[]
+	countArray = sizeof(MONEDAS) / sizeof(MONEDAS[0]);
+
+	//creamos un bucle para ordenar los numeros por el 
+	// metodo de insercion
+	for (i = 0; i < countArray; i++)
+	{
+		pos = i;
+		aux = MONEDAS[i];
+		while ((pos > 0) && (aux < MONEDAS[pos-1]))
+		{
+			MONEDAS[pos] = MONEDAS[pos-1];
+			pos--;
+		}
+		MONEDAS[pos] = aux;
+	}
+	
+	
+	
+    return 0;
+}
