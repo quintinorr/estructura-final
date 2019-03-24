df9407947d971a30257b80c7b8d01f9a74f1b55d
Javier ercedes <jm@prixet.com>
2019-03-24 11:59:53 -0400

    modifidado cajero.c para mostrar en Windows

--- cajero/c/cajero.c
+++ cajero/c/cajero.c
@@ -1,7 +1,8 @@
+/*
+Grupo 5 Estructura de datos.
+Encontrar el mínimo número de Billetes y/o monedas para representar una cantidad de dinero dada.
+Creado por Javier Mercedes.
+*/
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World\n");
-    return 0;
-}
+double MONEDAS[13] = {1000, 2000, 200, 100, 50, 20, 10, 5, 1, (25/100), (10/100), (5/100), (1/100)};
