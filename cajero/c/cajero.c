d2f26c0f5b45980f785774ead3776cfb7b1e2d01
Javier Mercedes <manuelmercedez10@gmail.com>
2019-03-31 19:31:20 -0400

    modified cajero.c

--- cajero/c/cajero.c
+++ cajero/c/cajero.c
@@ -8,12 +8,14 @@ Creado por Javier Mercedes.
 
 int main(void)
 {
-	
+	//Primero, creamos un array con los billetes que vamos a utilizar
    	int denominacion[] = {2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
+   	// creamos las variables a utilizar durante el proceso
    	int cantidad;
    	int resultado;
    	int i;
    
+   	//Pedimos al usuario que ingrese la cantidad a calcular
    	printf("Introduce la cantidad a calcular: ");
    	fflush(stdout);
    	if (scanf("%d", &cantidad) != 1 || cantidad < 1)
