abe9981e1e9b2ba094d1273fca0e17de50c839c4
quintinorr <46490491+quintinorr@users.noreply.github.com>
2019-04-05 15:51:16 -0400

    Mejoras con movimiento
    Mejoras con movimiento pero sin iteraccion

--- ajadrez/ajedrez.js
+++ ajadrez/ajedrez.js
@@ -1,54 +1,150 @@
 let queenRow = 1;
-let queenCol = 6;
-let moveX, moveY;
+function getMovementsAllowedQueen(queenCol, queenRow){
+  
+  let position = {
+    col : null,
+    row : null
+  };
 
-moveX = queenRow;
-moveY = queenCol;
+  let movementsAllowedoQueen = [];
+  let moveX, moveY;
 
-let diagonalLeftRow =  0;
-let diagonalLeftCol=   moveY - (moveX);
+  moveX = queenRow;
+  moveY = queenCol;
 
-let diagonalRightCol = (moveX) + moveY;
-let diagonalRightRow = 0;
+  let diagonalLeftRow =  0;
+  let diagonalLeftCol=   moveY - (moveX);
 
-for(let row = 0; row < 9 ;row++)
-{
-    let text='';
+  let diagonalRightCol = (moveX) + moveY;
+  let diagonalRightRow = 0;
 
-    for(let col  = 0; col <  9; col++ ){
-        if(col == 0 && row != 0){
-            text += row.toString() + ' ';
+  for(let row = 0; row < 9 ;row++)
+  {
+      for(let col  = 0; col <  9; col++ ){
+
+        let isAllowed = false;
+
+          if(moveY == col){
+            isAllowed = true;
+          }
+          else if(row == moveX){
+            isAllowed = true;
+          }
+          else if(diagonalLeftRow == row && col == diagonalLeftCol){
+            isAllowed = true;
+          }
+          else if(diagonalRightRow == row && col == diagonalRightCol){
+            isAllowed = true;           
+          }
+
+        if (isAllowed){
+          position ={
+            col : col,
+            row : row
+          };
+          movementsAllowedoQueen.push(position);
         }
-        else if(row == 0 && col != 0){
-            text += col.toString() + ' ';
-        }
-        else if (col == moveY && row == moveX){
-            text +='R ';
-        }
-        else if(moveY == col){
-            text +='* ';
-        }
-        else if(row == moveX){
-            text +='* ';
-        }
-        else if(diagonalLeftRow == row && col == diagonalLeftCol){
-            text +='* ';
+      }
 
 
-        }
-        else if(diagonalRightRow == row && col == diagonalRightCol){
-            text +='* ';
-        }
-        else{
-            text +='+ ';
-        }
+      diagonalLeftRow++;
+      diagonalLeftCol++;
+
+      diagonalRightCol--;
+      diagonalRightRow++;
+  }
+
+  return movementsAllowedoQueen;
+}
+
+
+
+function drawTable(queenPositionCol, queenPositionRow) {
+
+let movementsAllowedQueen = [];
+
+movementsAllowedQueen = getMovementsAllowedQueen(queenPositionCol,queenPositionRow);
+
+  for(let roq = 0; roq < 9; roq++){
+
+    let text = '';
+
+    for(let col = 0; col < 9 ; col++){
+
+      let allow= movementsAllowedQueen.find(x=> x.row == roq && x.col == col);
+
+      if(roq == 0 && col == 0){
+        text += 'x ';
+      }
+      else if(roq == 0 && col != 0){
+        text += col.toString() + ' ';
+      }
+      else if(col == 0 && roq != 0){
+        text += roq.toString() + ' ';
+      }
+      else if (col == queenPositionCol && roq == queenPositionRow){
+        text +='R ';
+      }
+      else if (allow){
+        text +='* ';
+      }      
+      else{
+        text +='+ ';
+      }
+
     }
 
-    diagonalLeftRow++;
-    diagonalLeftCol++;
-
-    diagonalRightCol--;
-    diagonalRightRow++;
-
     console.log(text);
+
+  }
+
+};
+
+function readNewPosition ()
+{
+  let position = {
+    col : 7,
+    row :5
+  };
+
+  return position;  
 }
+
+function readOpcion()
+{
+  let numero = 1;
+  return numero;    
+}
+
+
+function play()
+{
+  let queenInitialposition = {
+    col : 4,
+    row : 8
+  };
+
+  drawTable(queenInitialposition.col, queenInitialposition.row);
+
+  let opcion = 1;
+  do {
+    console.log('¿Desea mover a la reyna?')
+    console.log('1: Si');
+    console.log('2: No');
+
+    opcion = readOpcion();
+
+    if(opcion == 1)
+    {
+      let newPosition = readNewPosition();
+      console.clear();
+      drawTable(newPosition.col, newPosition.row);
+    }
+
+  }
+  while (opcion != 2);
+
+
+}
+
+/*play();*/
